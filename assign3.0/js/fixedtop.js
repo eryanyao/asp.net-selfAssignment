8cf8ba0b5dba7f7a19353386d78b5cac50257ec1
ER YAN YAO <yanyao1999@outlook.com>
2021-03-24 10:27:58 +0800

    Initial commit

---
+++ assign3.0/js/fixedtop.js
@@ -0,0 +1,13 @@
+﻿
+var nav = document.getElementById('top');
+
+window.onscroll = function () {
+    if (window.pageYOffset > 10) {
+        nav.style.position = "fixed";
+        nav.style.top = "0px";
+    }
+    else {
+        nav.style.position = "absolute";
+        nav.style.top = "0px";
+    }
+}
